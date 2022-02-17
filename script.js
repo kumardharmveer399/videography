b43af541564f21a7f1a4e8ae3c013c43483a6ed8
Dharmveer Mahtha <kumardharmveer399@gmail.com>
2022-02-17 21:50:16 +0530

    video

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function scrollbg() {
+var scrollvalue = window.scrollY;
+var navhead=document.getElementById("navhead");
+if (scrollvalue < 45 ) {
+    navhead.classList.remove('sticky');
+    document.getElementsByClassName('ntog').style.backgroundColor = "red";
+  
+} else {
+    navhead.classList.add('sticky');
+  }
+// console.log(scrollvalue);
+}
+window.addEventListener('scroll', scrollbg)
