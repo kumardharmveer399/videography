7b3754d32c569e8cdf14c7f099fa2aa6d34a7736
Dharmveer Mahtha <kumardharmveer399@gmail.com>
2022-03-03 22:06:03 +0530

    abc

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 function scrollbg() {
-var scrollvalue = window.scrollY;
-var navhead=document.getElementById("navhead");
-if (scrollvalue < 45 ) {
+  var scrollvalue = window.scrollY;
+  var navhead = document.getElementById("navhead");
+  if (scrollvalue < 45) {
     navhead.classList.remove('sticky');
     document.getElementsByClassName('ntog').style.backgroundColor = "red";
-  
-} else {
+
+  } else {
     navhead.classList.add('sticky');
   }
-// console.log(scrollvalue);
+  // console.log(scrollvalue);
 }
 window.addEventListener('scroll', scrollbg)
